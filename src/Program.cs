9764a084a9b3737e02eb257c10ef614ba6db9513
Samuel Nilsson <80719133+samuelnilsson06@users.noreply.github.com>
2021-06-19 22:08:43 +0200

    Add files via upload

---
+++ src/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CommandParsing;
+
+
+namespace Main
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Title = "Command Parsing system";
+            TakeInput();
+        }
+        public static void TakeInput()
+        {
+            string input = Console.ReadLine();
+
+            commandParse.HandleInput(input);
+        }
+    }
+}
