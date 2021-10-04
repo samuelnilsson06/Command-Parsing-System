966ac2e4bd5a1e43844c8ee69164a824151b7a36
Samuel Nilsson <80719133+samuelnilsson06@users.noreply.github.com>
2021-10-04 17:20:15 +0200

    better..
    im tired, i dont want to write long description
    
    Less code 
    better comments(i think)
    Looks better
    idk

---
+++ src/InputManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace The_CLI
+{
+    class InputManager
+    {
+        public static void TakeInput()
+        {
+            Console.Write($"{Environment.UserName}>");
+            string inpt = Console.ReadLine();
+            InputManager.Handle(inpt);
+        }
+        public static void Handle(string Inpt)
+        {
+            Inpt = Inpt.Trim();
+            if (Inpt == "")
+            {
+                Console.WriteLine("No Input was given");
+            }
+
+            string cmd;
+            try
+            {
+                cmd = Inpt.Substring(0, Inpt.IndexOf(' '));
+            }
+            catch
+            {
+                cmd = Inpt;
+            }
+
+            
+            if (typeof(Commands.cmd).GetMethod(cmd) != null)
+            {
+                // Does command contain arguments (Cant be "[the cmd][space]" because of Trim())
+                if (Inpt.Contains(" "))
+                {
+                    string[] args = Inpt.Substring(Inpt.IndexOf(' ') + 1).Split(' ');
+
+                    //Get the mothod name of cmds, then create new instance and pass it the argument
+                    typeof(Commands.cmd).GetMethod(cmd).Invoke(new Commands.cmd(), new[] { args });
+                }
+                else
+                {
+                    //else pass it an empty string in the array
+                    typeof(Commands.cmd).GetMethod(Inpt).Invoke(new Commands.cmd(), new[] { new string[] { "" } });
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine("command not found");
+            }
+        }
+    }
+    
+}
---
+++ src/cmds.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Commands
+{
+    class cmd
+    {
+        // How to Use //
+
+        //1. Create a method with the name of your desired command.
+        //2. Your arguments will be packed in a string array
+        //3. Write your code
+
+        public static void cmd1(string[] args)
+        {
+            Console.WriteLine("cmd1");
+        }
+        public static void cmd2(string[] args)
+        {
+            Console.WriteLine("cmd2");
+        }
+        public static void cmd3(string[] args)
+        {
+            Console.WriteLine("cmd3");
+        }
+    }
+}
